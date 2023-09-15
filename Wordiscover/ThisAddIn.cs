9c912d8b9b8d9d3eb7b9f1a712ed232b6d57daf7
Nick Sorrell <nick@cint.io>
2023-09-15 08:43:25 -0400

    Some changes that let editable docs have multiple instances of Wordiscover

--- Wordiscover/ThisAddIn.cs
+++ Wordiscover/ThisAddIn.cs
@@ -17,23 +17,55 @@ namespace Wordiscover
     {
         CustomTaskPane myCustomTaskPane;
         FindPane findPane;
+        Dictionary<Word.Document, CustomTaskPane> docToPaneMapping;
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             findPane = new FindPane();
             myCustomTaskPane = this.CustomTaskPanes.Add(findPane, "Wordiscover");
-            
+            docToPaneMapping = new Dictionary<Word.Document, CustomTaskPane>();
+
             this.Application.DocumentBeforeClose += new Word.ApplicationEvents4_DocumentBeforeCloseEventHandler(ClosePaneOnDocClose);
             this.Application.DocumentChange += new Word.ApplicationEvents4_DocumentChangeEventHandler(DocHasChanges);
             this.Application.DocumentOpen += new Word.ApplicationEvents4_DocumentOpenEventHandler(DocLoaded);
+            this.Application.WindowActivate += new Word.ApplicationEvents4_WindowActivateEventHandler(WindowActivated);
+            this.Application.ProtectedViewWindowActivate += new Word.ApplicationEvents4_ProtectedViewWindowActivateEventHandler(ProtectedWindowActivated); 
         }
 
         private void DocLoaded(Word.Document Doc)
         {
+
             findPane.GetParagraphMarkers();
             findPane.DocumentHasChanges = false;
         }
 
+        private void ProtectedWindowActivated(Word.ProtectedViewWindow PvWn)
+        {
+            var doc = PvWn.Document;
+            if (!docToPaneMapping.ContainsKey(doc))
+            {
+                addPaneToDictionary(doc);
+            }
+            myCustomTaskPane = docToPaneMapping[doc];
+            myCustomTaskPane.Visible = true;
+        }
+
+        private void WindowActivated(Word.Document Doc, Word.Window Wn)
+        {
+            if (!docToPaneMapping.ContainsKey(Doc))
+            {
+                addPaneToDictionary(Doc);
+            }
+            myCustomTaskPane = docToPaneMapping[Doc];
+            myCustomTaskPane.Visible = true;
+        }
+
+        private void addPaneToDictionary(Word.Document Doc)
+        {
+            findPane = new FindPane();
+            CustomTaskPane ctp = this.CustomTaskPanes.Add(findPane, "Wordiscover", Doc.ActiveWindow);
+            docToPaneMapping[Doc] = ctp;
+        }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
@@ -46,15 +78,25 @@ namespace Wordiscover
 
         private void ClosePaneOnDocClose(Word.Document Doc, ref bool b)
         {
-            if (myCustomTaskPane.Visible)
-                myCustomTaskPane.Visible = false;
+            if (docToPaneMapping.ContainsKey(Doc))
+            {
+                CustomTaskPane ctp = docToPaneMapping[Doc];
+                this.CustomTaskPanes.Remove(ctp);
+                docToPaneMapping.Remove(Doc);
+            }
+
+            //if (myCustomTaskPane.Visible)
+            //    myCustomTaskPane.Visible = false;
         }
         public void ToggleFindPane()
         {
-            if (this.Application.ActiveDocument != null)
+            if (this.Application.ProtectedViewWindows.Count > 0 || (this.Application.Documents.Count > 0 && this.Application.ActiveDocument != null))
                 myCustomTaskPane.Visible = !myCustomTaskPane.Visible;
             else
+            {
                 MessageBox.Show("You must have a document open to use Wordiscover.");
+            }
+                
         }
 
 
